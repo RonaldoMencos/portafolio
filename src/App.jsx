74dea7343b5becd85e6f1f5802673177e88c4d0e
Oscar Mencos <Oscar.Mencos@gentrac.com.gt>
2022-04-22 11:53:42 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='mx-auto text-center'>
+      <h1 className='text-3xl font-black'>
+        I'm Oscar Ronaldo Mencos</h1>
+    </div>
+  )
+}
+
+export default App
